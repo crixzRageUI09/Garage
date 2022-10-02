04596446e088d4f8ea2d4867a2cc429d72347f90
crixz <69543609+crixzRageUI@users.noreply.github.com>
2022-10-03 00:22:51 +0200

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,22 @@
+Config = {
+    poundprice = 100, -- Prix à payer pour la fourrière
+
+    sortie = {
+        {x = 227.63, y = -804.28, z = 29.60}, -- Point pour sortir les véhicules
+        {x = -1905.99, y = 2008.34, z = 142.60}, -- Vigne
+        {x = 1500.73, y = 3762.17, z = 33.00}, -- Lac Sandy
+        {x = 126.07, y = 6589.77, z = 31.00}, -- Station nord
+    },
+
+    rentrer = {
+        {x = 212.71, y = -798.02, z = 29.90}, -- Point pout rentrer les véhicules
+        {x = -1905.90, y = 2017.19, z = 140.00}, -- Vigne
+        {x = 1494.86, y = 3758.78, z = 33.00}, -- Lac Sandy
+        {x = 137.28, y = 6580.33, z = 31.10}, -- Station nord
+    },
+
+    pound = {
+        {x = 402.05, y = -1632.43, z = 28.35}, -- Point de la fourrière LS
+        {x = -474.81, y = 6029.85, z = 30.40}, -- Point de la fourrière NORD
+    },
+}
